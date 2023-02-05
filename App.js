24f61c0e4d7d9eda212d75a531dc5bb9ee09fc6c
rhams1997 <88038755+rhams1997@users.noreply.github.com>
2023-02-05 14:31:32 -0300

    commit

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import React from 'react';
+
+
+import { Home } from './src/screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Olá, Mundo!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="light-content"/>
+      <Home/>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0000ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+import { styles } from './styles'
+export  function Home() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>
+        React Native is on!
+      </Text>
+    </SafeAreaView>
+  );
+}
