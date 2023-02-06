85b3ccfc79cbdae0d7bee49f98d114d265161915
rhams1997 <88038755+rhams1997@users.noreply.github.com>
2023-02-05 23:29:03 -0300

    commit

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import { Text, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles'
 export  function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.text}>
-        React Native is on!
+        Olá, Rafael
       </Text>
+      <TextInput 
+        style={styles.input}
+        placeholder='Digite sua tarefa...'
+        placeholderTextColor='#555555'
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>
+          Adicionar
+        </Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
+
+//// TextInput para entrada de texto
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -8,6 +8,28 @@ export const styles = StyleSheet.create({
   
     text: {
       color: 'white',
-      fontsize: 32
+      fontsize: 32,
+      margin: 20
+    },
+    input:{
+      backgroundColor: '#1F1E25',
+      color: 'white',
+      fontSize: 18,
+      padding: Platform.OS == 'ios' ? 15 : 10,
+      marginHorizontal: 20
+    },
+    button: {
+      backgroundColor: 'green',
+      padding: 15,
+      margin: 20,
+      alignItems: 'center',
+      borderRadius: 8
+    },
+    buttonText: {
+      color: 'white',
+      fontSize: 18,
+      fontWeight: 'bold'
     }
 });
+
+//Platform.OS permite testar qual sistema operacional está ativo, para relizar configurações especificas de ambiente
