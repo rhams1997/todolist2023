d580036609c7f3d2cdbe6878e7326526655de897
rhams1997 <88038755+rhams1997@users.noreply.github.com>
2023-02-07 10:37:47 -0300

    Update index.js

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles'
 export  function Home() {
+
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function handleAddNewTask(){
+    setTasks(oldState => [...oldState, newTask]);
+
+    console.log(tasks);
+  }
+
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.text}>
@@ -12,15 +23,21 @@ export  function Home() {
         style={styles.input}
         placeholder='Digite sua tarefa...'
         placeholderTextColor='#555555'
+        onChangeText={setNewTask}
       />
       <TouchableOpacity
         style={styles.button}
-        activeOperacity={0.7}
+        activeOperacity={0.7}//Efeito de toque
+        onPress={handleAddNewTask}
+      
       >
         <Text style={styles.buttonText}>
           Adicionar
         </Text>
       </TouchableOpacity>
+      <Text style={styles.text}>
+        Minhas Tarefas
+      </Text>
     </SafeAreaView>
   );
 }
