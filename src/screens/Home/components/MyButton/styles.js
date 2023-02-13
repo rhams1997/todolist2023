d588e765591b5773751cf09d2d435e34047647ee
rhams1997 <88038755+rhams1997@users.noreply.github.com>
2023-02-13 10:23:35 -0300

    Update styles.js

---
+++ src/screens/Home/components/MyButton/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    buttonText:{
+      backgroundColor: '#1F1E25',
+      padding: 15,
+      borderRadius: 22,
+      marginVertical: 10,
+      marginHorizontal: 20,
+      marginBottom: 10
+    },
+    textTask:{
+      color: 'white',
+      fontSize: 20
+    }
+});
+
+//Platform.OS permite testar qual sistema operacional está ativo, para relizar configurações especificas de ambiente
